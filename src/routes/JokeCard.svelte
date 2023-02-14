9623c11e90960ed5196b2ff99402ff161af6a03c
Flajt <matrimkurz@gmail.com>
2023-02-14 16:15:21 +0100

    Fix+Chore: Adjust size for mobile, formatting

--- src/routes/JokeCard.svelte
+++ src/routes/JokeCard.svelte
@@ -4,17 +4,16 @@
 	export let joke: JokeModel;
 
 	async function handleBookMark() {
-		joke.hasBookmarked=!joke.hasBookmarked;
+		joke.hasBookmarked = !joke.hasBookmarked;
 		if (joke.hasBookmarked == true) {
 			const resp = await fetch('/api/bookmark', {
 				method: 'POST',
 				credentials: 'include',
 				body: JSON.stringify({ id: joke.id })
 			});
-			if(!resp.ok){
-				alert("Please log in to bookmark jokes")
-				joke.hasBookmarked=!joke.hasBookmarked;
-
+			if (!resp.ok) {
+				alert('Please log in to bookmark jokes');
+				joke.hasBookmarked = !joke.hasBookmarked;
 			}
 		} else {
 			const resp = await fetch('/api/bookmark', {
@@ -23,9 +22,9 @@
 				body: JSON.stringify({ id: joke.id })
 			});
 			if (resp.ok) {
-				myJokesStore.update((v)=>v.filter((v)=>v.id!==joke.id));
-			}else{
-				joke.hasBookmarked=!joke.hasBookmarked;
+				myJokesStore.update((v) => v.filter((v) => v.id !== joke.id));
+			} else {
+				joke.hasBookmarked = !joke.hasBookmarked;
 			}
 		}
 	}
@@ -45,7 +44,8 @@
 				await handleBookMark();
 			}}
 			data-cy="bookmark"
-			id="bookmark-btn">
+			id="bookmark-btn"
+		>
 			<img
 				data-cy={joke.hasBookmarked}
 				src={!joke.hasBookmarked ? './bookmark-white.png' : './bookmark.png'}
@@ -57,6 +57,10 @@
 </div>
 
 <style>
+	* {
+		box-sizing: border-box;
+	}
+
 	.jokeCard {
 		border-radius: 8px;
 		border: 2px solid black;
@@ -65,7 +69,7 @@
 		padding: 8px;
 		margin: 8px;
 		box-shadow: 2px 2px 16px 2px grey;
-		max-height: 300px;
+		max-height: 350px;
 		min-height: 200px;
 	}
 	h2 {
@@ -85,8 +89,7 @@
 		display: grid;
 		justify-content: center;
 		align-content: center;
-		max-height: 300px;
-		min-height: 100px;
+		height: 20vh;
 		margin-bottom: 0px;
 	}
 	img {
@@ -98,13 +101,36 @@
 		display: inline-flex;
 		justify-content: end;
 		width: 100%;
-		height: 10vh;
-		min-height: 50px;
-		max-height: 100px;
+		height: 50px;
 	}
 	#bookmark-btn {
 		height: 50px;
 		background-color: transparent;
 		border: 0;
+	}
+	@media screen and (max-width: 450px) {
+		h2{
+			max-width: 95%;
+		}
+		.jokeCard {
+			min-width: 250px;
+		}
+		#setup{
+			max-height: 150px;
+			overflow: hidden;
+			white-space: pre-line;
+			text-overflow: ellipsis;
+		}
+		#delivery{
+			max-height: 125px;
+			overflow: hidden;
+			white-space: nowrap;
+			text-overflow: ellipsis;
+		}
+		#body {
+			text-overflow: ellipsis;
+			overflow: scroll;
+			min-height: 250px;
+		}
 	}
 </style>
